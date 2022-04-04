b91d5a5eb7ff6974ca0dfd6a2feab1d72caf203f
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-05 00:16:43 +0600

    home component style added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import pic from './bag.jpg'
+import './Home.css'
 const Home = () => {
+
+
     return (
         <div>
-            <h1>this is home</h1>
+            <div className='home-comp'>
+                <div>
+                    <h1 className='probably'>Probably</h1>
+                    <h1 className='the-best'>the best reviewing site...</h1>
+                </div>
+                <div className='bag-img'>
+                    <img src={pic} alt="bag" />
+                </div>
+            </div>
+            <div>
+                <h1>Top Reviews</h1>
+            </div>
+
         </div>
     );
 };
