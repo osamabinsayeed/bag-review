8a2ce08e5c35ebe05cefbbc2df796516d83e3760
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-04 21:27:56 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
 
+      <nav>
+
+
+      </nav>
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
