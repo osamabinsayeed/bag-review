c7152e28c343181c83471377de10c174f4602767
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-04 22:18:19 +0600

    header styles added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Reviews from './Components/Reviews/Reviews'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
 
-      <nav>
-
-
-      </nav>
       <Routes>
-
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>} ></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h2>this is header</h2>
+
+            <nav className='header'>
+                <Link to="/home"><h5>Home</h5></Link >
+                <Link to="/reviews"><h5>Reviews</h5></Link>
+                <Link to="/dashboard"><h5>Dashboard</h5></Link>
+                <Link to="/blogs"><h5>Blogs</h5></Link>
+                <Link to="/about"><h5>About</h5></Link>
+            </nav>
         </div>
     );
 };
