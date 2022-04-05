71e826d15ad19d7fe65d2e7bb7eecc93f0a9601e
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-05 21:25:56 +0600

    questions answered

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>this is bologs</h1>
+
+            <h2>What is context api?</h2>
+            <p>Context API: Context API is a way to pass data from a prop without passing the data manually through every level of the component tree. We can skip props drilling using this</p>
+            <h2>What is semantic tag?</h2>
+            <p>Semantic Tag: Semantic Tag means the html tags that describes the meaning or purpose of the tag and property of the elements inside it to both browser and the developer.</p>
         </div>
     );
 };
