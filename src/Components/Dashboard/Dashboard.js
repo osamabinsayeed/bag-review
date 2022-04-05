5e282b1f0cc70be5d7407dc12f805593c58d539b
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-05 22:24:45 +0600

    charts added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,78 @@
 import React from 'react';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { BarChart, Bar, Legend } from 'recharts';
+import './Dashboard.css'
 const Dashboard = () => {
+
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ];
+
+    const renderLineChart = (
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="revenue" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="investment" />
+            <YAxis />
+            <Tooltip />
+        </LineChart>
+    );
+
+    const renderBarChart = (
+        <BarChart width={600} height={300} data={data}>
+            <XAxis dataKey="investment" stroke="#8884d8" />
+            <YAxis />
+            <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+            <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <Bar dataKey="sell" fill="#8884d8" barSize={30} />
+        </BarChart>
+    );
     return (
-        <div>
-            <h1>this is dashboard</h1>
+        <div className='charts'>
+            <div>
+                <h1>Line Chart: revenue vs investment</h1>
+                <div>{renderLineChart}</div>
+            </div>
+            <div>
+                <h1>Bar Chart: sell vs investment</h1>
+                <div>{renderBarChart}</div>
+            </div>
         </div>
     );
 };
