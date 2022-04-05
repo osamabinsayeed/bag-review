025e8bbbc010ad3e2d6cfd84c5425d9e996fc201
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-05 21:06:22 +0600

    see more reviews button added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,10 @@
 import pic from './bag.jpg'
 import './Home.css'
 import LoadData from '../hooks/LoadData';
+import { Link } from 'react-router-dom';
 const Home = () => {
     const reviews = LoadData().slice(0, 3);
-
+    const { name, review, img, id } = reviews;
     return (
         <div>
             <div className='home-comp'>
@@ -17,13 +18,17 @@ const Home = () => {
             </div>
             <div className='all-reviews'>
                 <h1>Top Reviews</h1>
-                {reviews.map(review => <div className='reviews'>
+                {reviews.map(review => <div className='reviews' key={review.id}>
                     <img src={review.img} alt="" />
                     <div>
-                        <h3>{review.name}</h3>
-                        <p className='text'>{review.review}</p>
+                        <h3 >{review.name}</h3>
+                        <p>{review.review}</p>
+                        <p>Rating:{review.rating}</p>
                     </div>
                 </div>)}
+                <div>
+                    <Link to="/reviews" className='more-reviews'><h2>See more reviews</h2></Link>
+                </div>
             </div>
 
         </div>
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Reviews.css'
+import LoadData from '../hooks/LoadData';
 const Reviews = () => {
+    const reviews = LoadData();
     return (
         <div>
-            <h1>this is review</h1>
+            <h1>All the reviews</h1>
+            {reviews.map(review => <div className='reviews' key={review.id}>
+                <img src={review.img} alt="" />
+                <div className='text'>
+                    <h3 >{review.name}</h3>
+                    <p>{review.review}</p>
+                    <p>Rating:{review.rating}</p>
+                </div>
+            </div>)}
         </div>
     );
 };
